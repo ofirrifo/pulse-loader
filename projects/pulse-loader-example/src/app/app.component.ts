2624dda89b027b507621e4e70e1f6fe80acbab49
ofirf <ofirf@sentinel.com>
2019-08-25 09:31:43 +0300

    edit example

--- projects/pulse-loader-example/src/app/app.component.ts
+++ projects/pulse-loader-example/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'pulse-loader-example';
 
   colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6'];
+  widths = ['6px', '13px', '20px', '25px'];
   pulseList = [];
 
   constructor() {
