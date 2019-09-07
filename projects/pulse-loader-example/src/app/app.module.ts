e5737566a6ffd185811d6ccc058da23b0a90a0c3
ofirf <ofirf@sentinel.com>
2019-09-07 11:42:01 +0300

    - enable ivy - add "rootDir": ".", to tsconfig.json

--- projects/pulse-loader-example/src/app/app.module.ts
+++ projects/pulse-loader-example/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PulseLoaderModule } from '../../../pulse-loader/src/lib/pulse-loader.module';
 
